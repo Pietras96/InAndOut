0f75b35a6508d113376277ace435601fccf24add
Piotrek <Piotrek@DESKTOP-1D3S46N>
2022-03-28 22:42:55 +0200

    Wstepna konfiguracja funkcji Edit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,58 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+        //get create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //post create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Item obj)
+        {
+            _db.Items.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            Item obj = _db.Items.Where(x => x.Id == Id).FirstOrDefault();
+            return View(obj);
+        }
+    }
+}
